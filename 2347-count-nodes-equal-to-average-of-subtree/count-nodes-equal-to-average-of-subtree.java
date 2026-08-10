a50f9e6b41ddd5f5a63e203ae3ae86dbb69aa080
Ashish-Jaiswal <ashishjaiswal9180@gmail.com>
2026-08-10 19:09:11 +0530

    Time: 1 ms (60.88%) | Memory: 45.6 MB (40.38%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -13,29 +13,71 @@
  *     }
  * }
  */
-class Solution {
-    int ans = 0;
-    int findSum(TreeNode root, int count[]) {
-        if(root == null) {
-            return 0;
-        }
-        count[0]++;
-        int leftSum = findSum(root.left, count);
-        int rightSum = findSum(root.right, count);
 
-        return root.val + leftSum + rightSum;
+
+                // Brute Force O(N2)
+// class Solution {
+//     int ans = 0;
+//     int findSum(TreeNode root, int count[]) {
+//         if(root == null) {
+//             return 0;
+//         }
+//         count[0]++;
+//         int leftSum = findSum(root.left, count);
+//         int rightSum = findSum(root.right, count);
+
+//         return root.val + leftSum + rightSum;
+//     }
+//     public int averageOfSubtree(TreeNode root) {
+//         if(root == null) {
+//             return 0;
+//         }
+//         int count[] = {0};
+//         int sum = findSum(root,count);
+//         if(root.val == sum/count[0]) {
+//             ans++;
+//         }
+//         averageOfSubtree(root.left);
+//         averageOfSubtree(root.right);
+//         return ans;
+//     }
+// }
+
+                        // Optimal solution
+
+// tc and sc = O(n)
+class Pair {
+    int sum;
+    int count;
+    Pair(int sum, int count) {
+        this.sum = sum;
+        this.count = count;
     }
-    public int averageOfSubtree(TreeNode root) {
+}
+    
+class Solution {    
+    int ans;
+
+    Pair solve(TreeNode root) {
         if(root == null) {
-            return 0;
+            return new Pair(0,0);
         }
-        int count[] = {0};
-        int sum = findSum(root,count);
-        if(root.val == sum/count[0]) {
+
+        Pair left = solve(root.left);
+        Pair right = solve(root.right);
+
+        int sum = root.val + left.sum + right.sum;
+        int count = 1 + left.count + right.count;
+
+        if(root.val == sum/count) {
             ans++;
         }
-        averageOfSubtree(root.left);
-        averageOfSubtree(root.right);
+
+        return new Pair(sum,count);
+    }
+    public int averageOfSubtree(TreeNode root) {
+        solve(root);
         return ans;
     }
+
 }
