e67b3846242e5644283109501078ff86bacfe996
Ashish-Jaiswal <ashishjaiswal9180@gmail.com>
2026-08-16 11:30:51 +0530

    Time: 1 ms (97.48%) | Memory: 47.6 MB (43.57%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,28 +2,28 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        
+        int row[] = new int[m];
+        int col[] = new int[n];
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(matrix[i][j] == 0) {
-                    map.putIfAbsent(i,new ArrayList<>());
-                    map.get(i).add(j);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-
-        for(int num : map.keySet()) {
-            for(int value : map.get(num)) {
-                for(int i=0; i<n; i++) {
-                matrix[num][i] = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                } 
             }
-            for(int i=0; i<m; i++) {
-                matrix[i][value] = 0;
-            }
-            }
-            
         }
+        
+
+        
         
      }
 }
